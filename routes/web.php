47deaab1270476a56f7ea33c5ec60e24d63325e0
Prince Yadav <princu2212@gmail.com>
2022-01-20 14:00:23 +0530

    Feat: filter reset adn desc order added

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,9 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [UserController::class, 'index'])->name('user');
+Route::get('/', [UserController::class, 'index']);
 Route::get('/create', [UserController::class, 'create'])->name('create');
 Route::post('/store', [UserController::class, 'store'])->name('store');
-// Route::get('/user', [UserController::class, 'getUser'])->name('user');
 Route::get('/edit{id}', [UserController::class, 'edit'])->name('edit');
 Route::put('/update{id}', [UserController::class, 'update'])->name('update');
 Route::delete('/destroy{id}', [UserController::class, 'destroy'])->name('destroy');
